8eb7cf0f5b513c9d8cb2661bd3a98de6965b5842
nicomcc <nicolasmocaiber@hotmail.com>
2019-11-25 21:10:18 -0300

    primeiro implementação bem sucedida com lib

---
+++ test/TESTADOR.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "soft_timer.h"
+
+
+void setup()
+{
+ 
+//soft_timer_init;
+
+}
+
+void main()
+{
+	soft_timer_init(1);
+  //printf("teste2");
+
+	printf("Plus one: %d\n", foo(10)); //Prints 11
+	//return 0;
+}
+
+
+
