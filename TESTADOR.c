5b49d724d314d3a57d2bff237349446b81a302b1
nicomcc <nicolasmocaiber@hotmail.com>
2019-11-28 13:01:33 -0300

    Primeira versão finalizada!

--- TESTADOR.c
+++ TESTADOR.c
@@ -8,17 +8,23 @@
 
 soft_timer_t *tester;
 soft_timer_t *tester2;
-
+soft_timer_t *tester3;
 
 
 void tmr1(soft_timer_t *p_timer)
 {
-	printf("Primeiro Timer CallBack\n");
+	printf("First Timer CallBack\n");
 }
 void tmr2(soft_timer_t *p_timer)
 {
-	printf("Segundo Timer CallBack\n");
+	printf("Second Timer CallBack\n");
 }
+
+void tmr3(soft_timer_t *p_timer)
+{
+	printf("Third Timer CallBack\n");
+}
+
 
 void setup()
 {
@@ -26,9 +32,11 @@ soft_timer_init();
 
 soft_timer_create(&tester);
 soft_timer_create(&tester2);
+soft_timer_create(&tester3);
 
 soft_timer_set(tester, tmr1, 3000, true);
 soft_timer_set(tester2, tmr2, 1000, true);
+soft_timer_set(tester3, tmr3, 5000, true);
 }
 
 
@@ -37,14 +45,13 @@ void main()
 setup();	
 
 soft_timer_start(tester);
-//soft_timer_start(tester2);
+soft_timer_start(tester2);
+soft_timer_start(tester3);
+//soft_timer_destroy(&tester);
 
-//while(1)
-//{
-//hmcu_timer_irq_handler();
-//soft_timer_virtual_interrupt(tester);
-//soft_timer_virtual_interrupt(tester2);
-//}
+while(1)
+	hmcu_timer_irq_handler();
+
 
 }
 
--- soft_timer.c
+++ soft_timer.c
@@ -17,19 +17,23 @@
 
 uint8_t clock_count = 0, previous_clock_count = 0;  //checks if IRQ was called
 uint8_t total_timers = 0;  //check how many timers the program is using
+uint8_t arrayLenght = 0;
 
 /*****************************************************************************
  * Global variables.
  *****************************************************************************/
 
-struct soft_timer{
+struct soft_timer
+{
+	uint8_t timer_id;	 //timer id 
 	bool timer_start; 	 //timer start flag
 	uint32_t timer_cnt;	 //current timer count
 	uint32_t ms_time; 	 //able to count up to 4294967296 ms
 	bool repeat;       	 //timer repeat flag
 	uint8_t status; 	 //timer operation status. Check @ref soft_timer_status_t.
 	soft_timer_callback_t callback;  //callback function
-	}; soft_timer_t *timers[SOFT_TIMER_MAX_INSTANCES]; //array of pointers to be used in IRQ function
+};
+soft_timer_t* timers[SOFT_TIMER_MAX_INSTANCES]; //array of pointers to be used in IRQ function
 
 
 /*****************************************************************************
@@ -44,23 +48,26 @@ struct soft_timer{
    Due to the low frequency of the 1000Hz timer clock, we have a period of 1ms, minimum counting time. This minimum time of 1ms will be used to trigger other virtual timers interruption routine. The prescale used will be 0 (00), and the timer will only recharge for one cycle. That is, from its maximum value 0xffff.
 *********/
 void soft_timer_init(void)  
-	{
+{
 	uint16_t timer_ctrl = 0x0007;
 	uint16_t timer_cnt = 0xFFFF;
 	uint16_t timer_rld = 0xFFFF;
-	} 
+	uint8_t i;
+	for (i = 0; i < SOFT_TIMER_MAX_INSTANCES; i++)
+	{
+		timers[i] = NULL;
+	}
+} 
 
 
 //create and allocate memory for virtual timer//
 void soft_timer_create(soft_timer_t **pp_timer)  
 {
-	 if (total_timers < SOFT_TIMER_MAX_INSTANCES - 1)
+	if (total_timers < SOFT_TIMER_MAX_INSTANCES - 1)
 	{
-        *pp_timer = malloc(sizeof(void));
-	pp_timer = (soft_timer_t **)malloc(1 * sizeof(soft_timer_t *));    
-	timers[total_timers] = pp_timer;
-
-	total_timers++;
+		*pp_timer = malloc(sizeof(void));
+		pp_timer = (soft_timer_t **)malloc(sizeof(soft_timer_t *));
+		total_timers++;
 	}
 	else
 		printf("Maximum number of timers has been reached.");
@@ -81,7 +88,7 @@ soft_timer_status_t soft_timer_set(soft_timer_t          *p_timer,
 	if (reload_ms < 0 || reload_ms > SOFT_TIMER_MAX_RELOAD_MS)  //out of timer range
 		return p_timer->status = SOFT_TIMER_STATUS_INVALID_STATE;
 
-	if(timeout_cb == NULL)	  				   //invalid callback function                   
+	if (timeout_cb == NULL)	  				   //invalid callback function                   
 		return p_timer->status = SOFT_TIMER_STATUS_INVALID_PARAMETER;
 	
 	p_timer->timer_start = false;
@@ -89,6 +96,9 @@ soft_timer_status_t soft_timer_set(soft_timer_t          *p_timer,
 	p_timer->ms_time = reload_ms;
 	p_timer->repeat = repeat;
 	p_timer->callback = timeout_cb;
+	p_timer->timer_id = arrayLenght;
+	timers[arrayLenght] = p_timer;
+	arrayLenght++;
 
 	return p_timer->status = SOFT_TIMER_STATUS_SUCCESS;	
 }
@@ -96,29 +106,33 @@ soft_timer_status_t soft_timer_set(soft_timer_t          *p_timer,
 //starts virtual timer//
 soft_timer_status_t soft_timer_start(soft_timer_t *p_timer)  
 {
-	p_timer->timer_start = true;
-	p_timer->timer_cnt = 0; 
-	
-	timers[0]->callback(timers[0]);	
+	if (p_timer->status == SOFT_TIMER_STATUS_SUCCESS)
+	{
+		p_timer->timer_start = true;
+		p_timer->timer_cnt = 0;	
+	}
 
-	return p_timer->status = SOFT_TIMER_STATUS_SUCCESS;
+	return p_timer->status;
 		
 }
 
 //stops virtual timer//
 soft_timer_status_t soft_timer_stop(soft_timer_t *p_timer)
 {
-	p_timer->timer_start = false;
-	p_timer->timer_cnt = 0; 
-
-	return p_timer->status = SOFT_TIMER_STATUS_SUCCESS;
+	if (p_timer->status == SOFT_TIMER_STATUS_SUCCESS)
+	{
+		p_timer->timer_start = false;
+		p_timer->timer_cnt = 0; 
+	}
+	return p_timer->status;
 }
 
 
 //libera memoria utilizar no timer virtual
 void soft_timer_destroy(soft_timer_t **pp_timer)
 {
-	free(pp_timer);  
+	timers[(*pp_timer)->timer_id] = NULL;
+	free(*pp_timer);  
 	total_timers--; 
 }
 
@@ -129,13 +143,11 @@ void soft_timer_destroy(soft_timer_t **pp_timer)
 //called from IRQ routine to check virtual interruptions//
 void soft_timer_virtual_interrupt(soft_timer_t *p_timer)   
 {
-
 	if((clock_count != previous_clock_count) && p_timer->timer_start) //every time global counter increases (1ms)
 		p_timer->timer_cnt++;
 	
-	
 	if (p_timer->timer_cnt >= p_timer->ms_time)   //reaches timer goal
-	{	
+	{
 		p_timer->timer_cnt = 0; 
 		p_timer->callback(p_timer);
 		if(!p_timer->repeat)      //do only once if repeat is set to false
@@ -149,7 +161,15 @@ void hmcu_timer_irq_handler(void)
 {	
 	previous_clock_count = clock_count;
 	clock_count++;
-
+	uint8_t i;
+	for (i = 0; i < SOFT_TIMER_MAX_INSTANCES; i++)
+	{
+		if (timers[i] == NULL)
+		{
+			continue;
+		}
+		soft_timer_virtual_interrupt(timers[i]);
+	}
 	usleep(1000);  //used for testing purpose
 }
 
